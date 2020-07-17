0640b5f547e6250dd1e9821d3e79535e79ba38ab
David Whitney <david@davidwhitney.co.uk>
2020-07-17 16:26:28 +0100

    Vue countdown.

--- app/js/components/TimerBar.js
+++ app/js/components/TimerBar.js
@@ -1,39 +1,42 @@
 export const TimerBar = {
-    props: ['countdown'],     
+    props: ['countdown'], 
+
     data: function() {
       return {
         timeRemaining: null,
-        countdownTimer: null
+        countdownTimer: null,
+        hasCountdown: false
       }
     },  
     
     computed: {
-      seconds: function() {
-        return Math.floor(this.timeRemaining / 1000);
-      },
-      percentage: function() {
-        return Math.floor( this.timeRemaining / (this.countdown / 100));
-      }
+      seconds: function() { return Math.floor(this.timeRemaining / 1000); },
+      percentage: function() { return this.timeRemaining / (this.countdown / 100); }
     },
 
-    created: function () {      
+    created: function () { 
+      this.hasCountdown = !isNaN(this.countdown);     
       this.timeRemaining = this.countdown;
 
       this.countdownTimer = setInterval(() => {
-        this.timeRemaining -= 1000;
+        this.timeRemaining -= 50;
 
         if (this.timeRemaining <= 0) {
           this.timeRemaining = null;
+          this.hasCountdown = false;
           clearInterval(this.countdownTimer);
         }
-      }, 1000);      
+      }, 50);
+    },   
 
-    },
-   
-
-    template: `   
-<div v-if="timeRemaining != null">
-    Time left: {{ seconds }} {{ percentage }}
+    template: `
+<div v-if="hasCountdown">
+  Time left: {{ seconds }} <br />
+  <div style="width: 100%; background-color: black; padding: 5px;">
+    <div v-bind:style="{ width: percentage + '%', backgroundColor: 'red' }">
+      &nbsp;
+    </div>
+  </div>
 </div>
 `
 };
