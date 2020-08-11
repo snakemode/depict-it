b193ac67cd9c0d06fbfb08a87125688669ad7eef
thisisjofrank <jofranchetti@gmail.com>
2020-08-11 12:28:07 +0100

    remove orange box from loader

--- app/js/components/Loader.js
+++ app/js/components/Loader.js
@@ -1,6 +1,6 @@
 export const Loader = {
     template: `
-    <div class="loader game-lobby">
+    <div class="loader">
     <h2 class="section-heading">Creating a game</h2>
      <img src="../../assets/loading.gif" alt="loading" class="loader-gif" />
     </div>
